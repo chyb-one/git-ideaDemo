f9f5d18928feb828900a86767e089658acf688ee
chyb <chyb@123.com>
2025-03-25 09:14:11 +0800

    demo2

--- src/main/java/com/chyb/demo/test2.java
+++ src/main/java/com/chyb/demo/test2.java
@@ -3,5 +3,13 @@ package com.chyb.demo;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
+        System.out.println("this is test2");
     }
 }
