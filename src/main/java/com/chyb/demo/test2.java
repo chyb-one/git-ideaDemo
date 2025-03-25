35f29f10b7f554e1523e91d4933418b2458293bc
chyb-one <1470409882@qq.com>
2025-03-25 13:54:28 +0800

    gitHub-code

--- src/main/java/com/chyb/demo/test2.java
+++ src/main/java/com/chyb/demo/test2.java
@@ -15,5 +15,6 @@ public class test2 {
         System.out.println("I am master1");
         System.out.println("I am hot-fix2");
         System.out.println("push this is master");
+        System.out.println("this is gitHub code");
     }
 }
